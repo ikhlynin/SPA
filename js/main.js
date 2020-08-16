46e5fa709fb89fbcac934486ca71dc903093a6cb
Igor Finkel <igorfnkl@gmail.com>
2020-08-16 21:00:36 +0300

    final

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  $('.slider__inner, .news__slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+  });
+
+  $('select').styler();
+
+  $('.header__btn-menu').on('click', function(){
+    $('.menu ul').slideToggle();
+  });
+
+});
